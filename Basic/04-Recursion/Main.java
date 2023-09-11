5b6cbbd078e5bb078646fc8596408f7c7f3a6971
Rohan <rohanrk39357@gmail.com>
2023-09-11 18:06:47 +0530

    recursion

---
+++ Basic/04-Recursion/Main.java
@@ -0,0 +1,33 @@
+//Explanation
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        message();
+        message();
+        message();
+        message();
+        message();
+
+    }
+
+    static void message() {
+        System.out.println("Hello world");
+    }
+}
+
+
+//NumberExample
+public class NumberExample {
+
+    public static void main(String[] args) {
+        // write your code here
+        message();
+    }
+
+    static void message() {
+        System.out.println("Hello world");
+    }
+}
+
+
